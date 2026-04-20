5f65aae35ffa69d93c62e9ba2df26e72e8d59d96
kuuonkuuon <phamnguyenthienan8.1@gmail.com>
2026-04-20 11:09:11 +0700

    eslint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,9 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import unusedImports from "eslint-plugin-unused-imports";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import checkFile from "eslint-plugin-check-file";
 
 export default tseslint.config(
   {
@@ -25,11 +28,45 @@ export default tseslint.config(
     },
   },
   {
+    plugins: {
+      "unused-imports": unusedImports,
+      "simple-import-sort": simpleImportSort,
+      "check-file": checkFile,
+    },
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
+      // defaults
+      '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       "prettier/prettier": ["error", { endOfLine: "auto" }],
+      // vars
+      "no-unused-vars": "off", 
+      "unused-imports/no-unused-vars": [
+        "warn",
+        { "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
+      ],
+      // file
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "unused-imports/no-unused-imports": "error",
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,js}": "KEBAB_CASE",
+        },
+        { "ignoreMiddleExtensions": true }
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          "src/**/": "KEBAB_CASE",
+        }
+      ],
+      // code
+      "no-console": "warn",
+      "max-depth": ["error", 4],
+      '@typescript-eslint/explicit-function-return-type': ['warn', { allowExpressions: true }],
+      "eqeqeq" : ["error", "always", { "null": "ignore" }],
     },
   },
 );
