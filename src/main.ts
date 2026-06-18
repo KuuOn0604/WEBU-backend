30f7975305271834fa66bc5d1b7b64d4de26b026
kuuonkuuon <phamnguyenthienan8.1@gmail.com>
2026-06-18 21:12:55 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,10 @@ async function bootstrap(): Promise<void> {
   );
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
